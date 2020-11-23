b09b98c52716863f7f2dfba644607126e9863bce
dhanrz <dhanrz@pop-os.localdomain>
2020-11-23 13:10:30 +0530

    code

--- lab8/path.py
+++ lab8/path.py
@@ -6,27 +6,31 @@ class Network:
     def addlink(self, u, v, w):  
         self.matrix.append((u, v, w)) 
         
-    def printtable(self, dist,src):
+    def printtable(self, dist,src, out):
         print("Router Table entries for router {}".format(chr(ord('A')+src)))
-        print("{0}\t{1}".format("Destination", "Cost"))  
+        print("{0}\t{1}\t{2}".format("Dest", "Hop", "Out"))  
         for i in range(self.n):  
-            print("{0}\t{1}".format(chr(ord('A')+i), dist[i]))  
-        
+            print("{0}\t{1}\t{2}".format(chr(ord('A')+i), dist[i], out))  
+    #Bellman-Ford
     def solve(self, src):          
         dist = [99] * self.n 
         dist[src] = 0
-
+        flag = True
+        out = ""
         for _ in range(self.n - 1):  
             for u, v, w in self.matrix:  
-                if dist[u] != 99 and dist[u] + w < dist[v]:  
+                if dist[u] != 99 and dist[u] + w < dist[v]: 
+                        if flag:
+                            out = chr(ord('A')+dist[u]+w)
+                            flag = False
                         dist[v] = dist[u] + w  
-        self.printtable(dist,src)  
+        self.printtable(dist,src, out)  
 
 
 if __name__ == "__main__":
     matrix=[]
     n=int(input("Enter No. of Routers : "))
-    print("Enter the Adjacency Matrix :")
+    print("Enter the Router dist Matrix :")
     for i in range(n):
         row=list(map(int,input().split(" ")))
         matrix.append(row)
