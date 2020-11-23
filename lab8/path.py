74044ea045330fd7aa19489ff00198ddb965cc7a
dhanrz <dhanrz@pop-os.localdomain>
2020-11-23 12:32:05 +0530

    writeup n halfcode

--- lab8/path.py
+++ lab8/path.py
@@ -1,43 +1,41 @@
-class Graph:
-    def _init_(self,n):
+class Network:
+    def __init__(self,n):
         self.matrix=[]
         self.n=n 
         
-    def addEdge(self, u, v, w):  
+    def addlink(self, u, v, w):  
         self.matrix.append((u, v, w)) 
         
-    def printArr(self, dist,src):
-        print("Vector Table of {}".format(chr(ord('A')+src)))  
-        print("{0}\t{1}".format("Dest, HOP"))
+    def printtable(self, dist,src):
+        print("Router Table entries for router {}".format(chr(ord('A')+src)))
+        print("{0}\t{1}".format("Destination", "Cost"))  
         for i in range(self.n):  
             print("{0}\t{1}".format(chr(ord('A')+i), dist[i]))  
-      
-    # to find shortest path
-    def BellmanFord(self, src):  
+        
+    def solve(self, src):          
         dist = [99] * self.n 
         dist[src] = 0
-   
-        for _ in range(self.n - 1):   
+
+        for _ in range(self.n - 1):  
             for u, v, w in self.matrix:  
                 if dist[u] != 99 and dist[u] + w < dist[v]:  
                         dist[v] = dist[u] + w  
-        self.printArr(dist,src)
-def main():
+        self.printtable(dist,src)  
+
+
+if __name__ == "__main__":
     matrix=[]
-    print("Enter No. of Nodes : ")
-    n=int(input())
+    n=int(input("Enter No. of Routers : "))
     print("Enter the Adjacency Matrix :")
     for i in range(n):
-        m=list(map(int,input().split(" ")))
-        matrix.append(m)
-    g=Graph(n)
+        row=list(map(int,input().split(" ")))
+        matrix.append(row)
+    g=Network(n)
     for i in range(n):
         for j in range(n):
             if matrix[i][j]==1:
-                g.addEdge(i,j,1)
-
-    for _ in range(n):
-        g.BellmanFord(_)
-
-
-main()
+                g.addlink(i,j,1)
+    
+    for i in range(n):
+        g.solve(i)
+    
